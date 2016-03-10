b03f29dcd97a16b67e0510c51dddb4a370da1188
alex.gill <alex.gill@penna.com>
2016-03-10 14:56:55 +0000

    SumReplace for EntityFramework (fixes bug with calling Sum on empty collections in EF)

---
+++ src/AFG.ExpressionTools.Tests/SumReplacementTest.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using Ploeh.AutoFixture.Xunit2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace AFG.ExpressionTools.Tests
+{
+    public class SumReplacementTest
+    {
+        [Fact]
+        public void SumReturnsZeroForEmptyListWhenReplacedByNullableVersionWithSelector()
+        {
+            // arrange
+            Expression<Func<IEnumerable<int>, int>> getSum = xs => xs.Sum(x => x);
+            var list = Enumerable.Empty<int>();
+
+            // act
+            var result = getSum.FixSumExpression().Compile()(list);
+
+            // assert
+            result.Should().Be(0);
+        }
+
+        [Fact]
+        public void SumReturnsZeroForEmptyListWhenReplacedByNullableVersionWithNoSelector()
+        {
+            // arrange
+            Expression<Func<IEnumerable<int>, int>> getSum = xs => xs.Sum();
+            var list = Enumerable.Empty<int>();
+
+            // act
+            var result = getSum.FixSumExpression().Compile()(list);
+
+            // assert
+            result.Should().Be(0);
+        }
+
+        [Theory, AutoData]
+        public void SumReturnsSumWhenReplacedByNullableVersionWithSelector(List<int> list)
+        {
+            // arrange
+            Expression<Func<IEnumerable<int>, int>> getSum = xs => xs.Sum(x => x);
+
+            // act
+            var result = getSum.FixSumExpression().Compile()(list);
+
+            // assert
+            result.Should().Be(list.Sum());
+        }
+
+        [Theory, AutoData]
+        public void SumReturnsSumWhenReplacedByNullableVersionWithNoSelector(List<int> list)
+        {
+            // arrange
+            Expression<Func<IEnumerable<int>, int>> getSum = xs => xs.Sum();
+
+            // act
+            var result = getSum.FixSumExpression().Compile()(list);
+
+            // assert
+            result.Should().Be(list.Sum());
+        }
+    }
+}
---
+++ src/AFG.ExpressionTools/EntityFrameworkFixExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace AFG.ExpressionTools
+{
+    public static class EntityFrameworkFixExtensions
+    {
+        public static Expression<Func<TIn, TResult>> FixSumExpression<TIn, TResult>(this Expression<Func<TIn, TResult>> expr)
+        {
+            return EmptyCollectionSumFixer.Replace(expr);
+        }
+
+        class EmptyCollectionSumFixer : ExpressionVisitor
+        {
+            private EmptyCollectionSumFixer() { }
+
+            private static readonly IEnumerable<MethodInfo> SumMethods =
+                typeof(Enumerable)
+                    .GetMethods(BindingFlags.Public | BindingFlags.Static)
+                    .Where(x => x.Name == "Sum");
+
+            protected override Expression VisitMethodCall(MethodCallExpression node)
+            {
+                if (SumMethods.All(x => x.MetadataToken != node.Method.MetadataToken)
+                    || node.Method.ReturnType.IsGenericType)
+                    return base.VisitMethodCall(node);
+
+                var source = node.Arguments[0];
+                if (node.Arguments.Count == 2)
+                {
+                    var selector = node.Arguments[1] as LambdaExpression;
+                    return Create(source, selector.ReturnType, selector.Body, selector.Parameters.ToArray());
+                }
+                else
+                {
+                    var returnType = source.Type.GetGenericArguments()[0];
+                    var parameter = Expression.Parameter(returnType);
+                    return Create(source, returnType, parameter, parameter);
+                }
+            }
+
+            private static Expression Create(Expression source, Type returnType, Expression lambdaBody, params ParameterExpression[] lambdaParameters)
+            {
+                var sourceType = source.Type.GetGenericArguments()[0];
+                var nullableType = typeof(Nullable<>).MakeGenericType(returnType);
+                var nullableSumMethod = SumMethods
+                    .First(x => x.GetParameters().Count() == 2 && x.ReturnType == nullableType)
+                    .MakeGenericMethod(sourceType);
+
+                var nullableBody = Expression.Convert(lambdaBody, nullableType);
+                var nullableLambdaExpression = Expression.Lambda(nullableBody, lambdaParameters);
+
+                return Expression.Coalesce(
+                    Expression.Call(nullableSumMethod, source, nullableLambdaExpression),
+                    Expression.Constant(Activator.CreateInstance(returnType)));
+            }
+
+            public static Expression<Func<TIn, TResult>> Replace<TIn, TResult>(Expression<Func<TIn, TResult>> expr)
+            {
+                var result = new EmptyCollectionSumFixer().Visit(expr.Body);
+                return Expression.Lambda<Func<TIn, TResult>>(result, expr.Parameters);
+            }
+        }
+    }
+}
